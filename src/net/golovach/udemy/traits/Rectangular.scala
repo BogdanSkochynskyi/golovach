69b5df87397bd9f4c2650c40383a17fcb8df39be
bogdanskochynskyi <bogdanskochynskyi@gmail.com>
2015-10-14 13:17:55 +0300

    [12.3] Add trait Rectangular

---
+++ src/net/golovach/udemy/traits/Rectangular.scala
@@ -0,0 +1,10 @@
+package net.golovach.udemy.traits
+
+trait Rectangular {
+  def topLeft: Point
+  def bottomRight: Point
+
+  def left = topLeft.x
+  def right = bottomRight.x
+  def width = right - left
+}
