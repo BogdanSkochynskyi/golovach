f0e33ecfaa25e5e10cc407e5bdb288208eb36924
bogdanskochynskyi <bogdanskochynskyi@gmail.com>
2015-10-14 13:16:24 +0300

    [12.3] Add abstract class Component

---
+++ src/net/golovach/udemy/traits/Component.scala
@@ -0,0 +1,10 @@
+package net.golovach.udemy.traits
+
+abstract class Component {
+  def topLeft: Point
+  def bottomRight: Point
+
+  def left = topLeft.x
+  def right = bottomRight.x
+  def width = right - left
+}
