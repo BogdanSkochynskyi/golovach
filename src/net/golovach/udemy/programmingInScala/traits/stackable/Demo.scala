fc62a1d1ec2fd31bdd9b18a60bcc3792fcf14110
bogdanskochynskyi <bogdanskochynskyi@gmail.com>
2015-10-14 14:05:07 +0300

    [12.5] Rewrite demo to show how stackable modifications are work

--- src/net/golovach/udemy/programmingInScala/traits/stackable/Demo.scala
+++ src/net/golovach/udemy/programmingInScala/traits/stackable/Demo.scala
@@ -4,9 +4,12 @@ object Demo {
   def main(args: Array[String]) {
     val queue = new MyQueue // == val queue = new BasicIntQueue with Doubling
     val queue1 = new BasicIntQueue with Doubling
+    val queue2 = new BasicIntQueue with Doubling with Incrementing
     queue.put(10)
     queue1.put(10)
+    queue2.put(10)
     println(queue.get())
     println(queue1.get())
+    println(queue2.get())
   }
 }
