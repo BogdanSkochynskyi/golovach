9569fd3cde4fdd547bc1aef171c4b4ff86b28176
bogdanskochynskyi <bogdanskochynskyi@gmail.com>
2015-10-14 12:53:03 +0300

    [12.1] show how we can override trait methods

--- src/net/golovach/udemy/traits/Frog.scala
+++ src/net/golovach/udemy/traits/Frog.scala
@@ -5,5 +5,7 @@ trait HasLegs
 class Frog extends Animal with Philosophical with HasLegs{
 
   override def toString = "green"
-
+  override def philosophize(): Unit = {
+    println("It ain't easy being " + toString + "!")
+  }
 }
